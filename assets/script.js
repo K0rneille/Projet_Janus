2f5a3b382d97be5778b0a67c53a4600683dce343
Korneille <nathan.callewaert.22@gmail.com>
2026-01-14 18:40:25 +0100

    /reponsive burger menu

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,34 @@
 "use strict" ;
+
+
+const nav = document.querySelector(".nav") ;
+const button = document.querySelector(".burgermenu");
+const links = document.querySelectorAll(".nav__link");
+
+if (nav){
+
+    for (let link of links){
+        link.addEventListener("click", menuclose);
+    }
+        button.addEventListener("click", menuswitch);
+}
+
+function menuclose(event){
+    event.preventDefault();
+    console.log('caca')
+
+    if (nav.classList.contains("nav--open")){
+
+        nav.classList.remove("nav--open");
+    }
+
+
+}
+
+
+function menuswitch(event){
+    event.preventDefault();
+    console.log('caca')
+
+    nav.classList.toggle("nav--open");
+}
